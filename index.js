08567f9320a71b8706cd9d1416d17f82ee937f5a
aniruddhazore <112480870+aniruddhazore@users.noreply.github.com>
2024-10-03 11:43:07 +0530

    Added serialNumber in cattle

--- index.js
+++ index.js
@@ -924,7 +924,6 @@ app.post("/milk", async (req, res) => {
 app.get("/milk/:date", async (req, res) => {
   try {
     let { date } = req.params;
-    console.log(date)
     // Attempt to parse the date using moment (allowing various formats)
     const parsedDate = moment(date, ["YYYY-MM-DD", "MM/DD/YYYY", "DD-MM-YYYY"], true);
     
